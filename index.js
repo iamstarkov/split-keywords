ff0dd3d0f2671d8c701af1605ce0278f1cd4b95c
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-25 22:44:47 +0100

    implement thing

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-export default function splitKeywords(input) {
-  return input;
-}
+import R from 'ramda';
+
+// splitKeywords :: String -> [String]
+const splitKeywords = R.pipe(
+  R.defaultTo(''),
+  R.split(','),
+  R.map(R.trim),
+  R.filter(R.pipe(R.isEmpty, R.not))
+);
+
+export default R.unary(R.unless(R.isNil, splitKeywords));
