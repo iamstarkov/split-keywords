e9160962f338c018ee7449de31a625fda76547b3
Vladimir Starkov <iamstarkov@gmail.com>
2016-02-26 10:13:35 +0100

    reimplement test and tdd scripts

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
-/* eslint-env mocha */
-
-import { deepEqual } from 'assert';
+import test from 'ava';
 import splitKeywords from './index';
 
-it('should splitKeywords', () =>
-  deepEqual(splitKeywords('uni, corns'), ['uni', 'corns']));
+test('should splitKeywords', (t) =>
+  t.same(splitKeywords('uni, corns'), ['uni', 'corns']));
 
-it('should splitKeywords tricky', () =>
-  deepEqual(splitKeywords(',uni,, corns,meow,'), ['uni', 'corns', 'meow']));
+test('should splitKeywords tricky', (t) =>
+  t.same(splitKeywords(',uni,, corns,meow,'), ['uni', 'corns', 'meow']));
 
-it('should splitKeywords empty input', () =>
-  deepEqual(splitKeywords(), undefined));
+test('should splitKeywords empty input', (t) =>
+  t.same(splitKeywords(), undefined));
