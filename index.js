d37ef4fa86824da0647dc30ec579f6bd7516e0a0
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-15 10:47:47 +0100

    add pure ramda validation

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import R from 'ramda';
 
 // splitKeywords :: String -> [String]
-const splitKeywords = R.pipe(
-  R.unless(R.is(String), () => { throw new Error('Expected string, but got undefined'); }),
+const splitKeywords = R.unary(R.pipe(
+  R.unless(R.is(String), () => { throw new Error('`keywords` should be String'); }),
   R.split(','),
   R.map(R.trim),
   R.reject(R.isEmpty)
-);
+));
 
 export default splitKeywords;
